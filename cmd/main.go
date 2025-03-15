fee7483f036c8470c27167b214169d9bc0ae8452
hisshihi <hissoffc@gmail.com>
2025-03-15 21:03:16 +0500

    Добавлены файлы конфигурации для управления настройками приложения, включая параметры подключения к базе данных и адрес сервера. Обновлен основной файл для использования конфигурации из нового файла. Обновлены зависимости в go.mod и go.sum.

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,19 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/hisshihi/order-of-venhicles-services/internal/config"
 	"github.com/hisshihi/order-of-venhicles-services/internal/db"
 	api "github.com/hisshihi/order-of-venhicles-services/internal/db/api/handlers"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/order_services?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config file", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.DBAddress)
 	if err != nil {
 		log.Fatal("cannot start to server:", err)
 	}
