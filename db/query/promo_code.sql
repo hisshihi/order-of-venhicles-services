dff4835719b5cfd3c91811b00cd52c9f4d8b8aa3
hisshihi <hissoffc@gmail.com>
2025-03-15 17:37:43 +0500

    Добавлены SQL-запросы и функции для управления промокодами: создание, получение, обновление и удаление промокодов. Обновлен интерфейс Querier для поддержки новых операций с промокодами.

---
+++ db/query/promo_code.sql
@@ -0,0 +1,28 @@
+-- name: CreatePromoCode :one
+INSERT INTO promo_codes (partner_id, code, discount_percentage, valid_until)
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: GetPromoCodeByID :one
+SELECT * FROM promo_codes
+WHERE id = $1;
+
+-- name: GetPromoCodeByPartnerID :one
+SELECT * FROM promo_codes
+WHERE partner_id = $1;
+
+-- name: ListPromoCodes :many
+SELECT * FROM promo_codes
+ORDER BY created_at DESC
+LIMIT $1 OFFSET $2;
+
+
+-- name: UpdatePromoCode :one
+UPDATE promo_codes
+SET partner_id = $2, code = $3, discount_percentage = $4, valid_until = $5
+WHERE id = $1
+RETURNING *;
+
+-- name: DeletePromoCode :exec
+DELETE FROM promo_codes
+WHERE id = $1;
--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -11,22 +11,27 @@ import (
 
 type Querier interface {
 	CreateOrder(ctx context.Context, arg CreateOrderParams) (Order, error)
+	CreatePromoCode(ctx context.Context, arg CreatePromoCodeParams) (PromoCode, error)
 	CreateService(ctx context.Context, arg CreateServiceParams) (Service, error)
 	CreateServiceCategory(ctx context.Context, arg CreateServiceCategoryParams) (ServiceCategory, error)
 	CreateSubscription(ctx context.Context, arg CreateSubscriptionParams) (Subscription, error)
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
 	DeleteOrder(ctx context.Context, id int64) error
+	DeletePromoCode(ctx context.Context, id int64) error
 	DeleteService(ctx context.Context, id int64) error
 	DeleteServiceCategory(ctx context.Context, id int64) error
 	DeleteSubscription(ctx context.Context, id int64) error
 	DeleteUser(ctx context.Context, id int64) error
 	GetOrderByID(ctx context.Context, id int64) (Order, error)
+	GetPromoCodeByID(ctx context.Context, id int64) (PromoCode, error)
+	GetPromoCodeByPartnerID(ctx context.Context, partnerID int64) (PromoCode, error)
 	GetServiceByID(ctx context.Context, id int64) (Service, error)
 	GetServiceCategoryByID(ctx context.Context, id int64) (ServiceCategory, error)
 	GetSubscriptionByID(ctx context.Context, id int64) (Subscription, error)
 	GetUserByIDFromAdmin(ctx context.Context, id int64) (User, error)
 	GetUserByIDFromUser(ctx context.Context, id int64) (interface{}, error)
 	ListOrders(ctx context.Context, arg ListOrdersParams) ([]Order, error)
+	ListPromoCodes(ctx context.Context, arg ListPromoCodesParams) ([]PromoCode, error)
 	ListServiceCategories(ctx context.Context, arg ListServiceCategoriesParams) ([]ServiceCategory, error)
 	ListServices(ctx context.Context, arg ListServicesParams) ([]Service, error)
 	ListServicesByCategory(ctx context.Context, categoryID int64) ([]Service, error)
@@ -37,6 +42,7 @@ type Querier interface {
 	ListUsersByRole(ctx context.Context, role NullRole) ([]User, error)
 	ListUsersByUsername(ctx context.Context, dollar_1 sql.NullString) ([]User, error)
 	UpdateOrder(ctx context.Context, arg UpdateOrderParams) (Order, error)
+	UpdatePromoCode(ctx context.Context, arg UpdatePromoCodeParams) (PromoCode, error)
 	UpdateService(ctx context.Context, arg UpdateServiceParams) (Service, error)
 	UpdateServiceCategory(ctx context.Context, arg UpdateServiceCategoryParams) (ServiceCategory, error)
 	UpdateSubscription(ctx context.Context, arg UpdateSubscriptionParams) (Subscription, error)
